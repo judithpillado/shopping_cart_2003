0abf79eef8f3dc10da91b9ee6476087daeaa4541
Judith Pillado <judith.pillado@gmail.com>
2020-03-25 11:20:49 -0500

    Add test it exists

---
+++ test/shopping_cart_test.rb
@@ -0,0 +1,11 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/product'
+require './lib/shopping_cart'
+
+class ShoppingCartTest < Minitest::Test
+  def test_it_exists
+    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_instance_of ShoppingCart, product
+  end
+end 
