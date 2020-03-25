e21676cfa9a75fb2e5b7b1b84f5f4f63aa425416
Judith Pillado <judith.pillado@gmail.com>
2020-03-25 11:54:36 -0500

    Add add product method

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -1,9 +1,18 @@
 class ShoppingCart
   attr_reader :name, :capacity
+  attr_accessor :products
 
   def initialize(name_parameter, capacity)
     @name = name_parameter
     @capacity = capacity
+    @products = []
   end
 
+  def capacity
+    @capacity.to_i
+  end
+
+  def add_product(product)
+    @products << product
+  end
 end
