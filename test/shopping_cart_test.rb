df460d91aacb139f64d44625e3bd0e11ed2ff4b6
Judith Pillado <judith.pillado@gmail.com>
2020-03-25 11:39:17 -0500

    Add test it can add products

--- test/shopping_cart_test.rb
+++ test/shopping_cart_test.rb
@@ -6,6 +6,19 @@ require './lib/shopping_cart'
 class ShoppingCartTest < Minitest::Test
   def test_it_exists
     cart = ShoppingCart.new("King Soopers", "30items")
-    assert_instance_of ShoppingCart, product
+    assert_instance_of ShoppingCart, cart
   end
-end 
+
+  def test_it_has_prodcuts
+    cart = ShoppingCart.new("King Soopers", "30items")
+    assert_equal [], cart.products
+  end
+
+  def test_it_can_add_products
+    product1 = Product.new(:paper, 'toilet paper', 3.70, '10')
+    product2 = Product.new(:meat, 'chicken', 4.50, '2')
+    assert_equal product1, cart.add_product(product1)
+    assert_equal product2, cart.add_product(product2)
+  end
+
+end
